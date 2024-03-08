c194ee33c94ad4bb1cfd89ff0aacadb12c5935bd
UserUnkown00 <161431197+UserUnkown00@users.noreply.github.com>
2024-03-08 17:32:03 +0800

    Add files via upload

---
+++ MainPage/index.php
@@ -0,0 +1,108 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width-device-width, initial-scale-1.0">
+    <title>Main Page</title>
+
+
+    <link rel="stylesheet" href="mainpagestyle.css">
+    <link rel="stylesheet" href="buttonanimate.css">
+  </head>
+  <body>
+      <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="">News</a></li>
+          <li><a href="">Contact</a></li>
+          <li><a href="">About</a></li>
+        </ul>
+        <h1 class="h1mp">WELCOME TO SSAMS</h1>
+        <div class="logo">
+            <img src="SEMUJA.png" alt="SSAMS">
+        </div>
+        <div class="mainpage">
+                      <!-- Slideshow container -->
+          <div class="slideshow-container">
+
+          <!-- Full-width images with number and caption text -->
+          <div class="mySlides fade">
+            <img src="sunset1.jpeg" >
+          </div>
+
+          <div class="mySlides fade">
+            <img src="sunset2.jpg" >
+          </div>
+
+          <div class="mySlides fade">
+            <img src="sunset3.jpeg">
+          </div>
+          <div class="container">
+    <!-- Button 1 -->
+    <a href="login.php" class="button">
+        <div class="button__content">
+            <span class="button__text">Staff Dashboard</span>
+            <i class="ri-download-cloud-fill button__icon"></i>
+
+            <div class="button__reflection-1"></div>
+            <div class="button__reflection-2"></div>
+        </div>
+
+        <img src="star.png" alt="none" class="button__star-1">
+        <img src="star.png" alt="none" class="button__star-2">
+        <img src="circle.png" alt="none" class="button__circle-1">
+        <img src="circle.png" alt="none" class="button__circle-2">
+        <img src="diamond.png" alt="none" class="button__diamond">
+        <img src="triangle.png" alt="none" class="button__triangle">
+
+        <div class="button__shadow"></div>
+    </a>
+
+    <!-- Button 2 -->
+    <a href="adminLogin.php" class="button">
+        <div class="button__content">
+            <span class="button__text">Management Dashboard</span>
+            <i class="ri-download-cloud-fill button__icon"></i>
+
+            <div class="button__reflection-1"></div>
+            <div class="button__reflection-2"></div>
+        </div>
+
+        <img src="star.png" alt="none" class="button__star-1">
+        <img src="star.png" alt="none" class="button__star-2">
+        <img src="circle.png" alt="none" class="button__circle-1">
+        <img src="circle.png" alt="none" class="button__circle-2">
+        <img src="diamond.png" alt="none" class="button__diamond">
+        <img src="triangle.png" alt="none" class="button__triangle">
+
+        <div class="button__shadow"></div>
+    </a>
+
+    <!-- Button 3 -->
+    <a href="another_page.html" class="button">
+        <div class="button__content">
+            <span class="button__text">Admin Dashboard </span>
+            <i class="ri-download-cloud-fill button__icon"></i>
+
+            <div class="button__reflection-1"></div>
+            <div class="button__reflection-2"></div>
+        </div>
+
+        <img src="star.png" alt="none" class="button__star-1">
+        <img src="star.png" alt="none" class="button__star-2">
+        <img src="circle.png" alt="none" class="button__circle-1">
+        <img src="circle.png" alt="none" class="button__circle-2">
+        <img src="diamond.png" alt="none" class="button__diamond">
+        <img src="triangle.png" alt="none" class="button__triangle">
+
+        <div class="button__shadow"></div>
+    </a>
+</div>
+        </div>
+        <script src="mainpage.js"></script>
+</body>
+</html>
